a9e80a379e9f469cf0c5720cc9ff0c80499bfdf6
Fadel Harris <itsmefadel@gmail.com>
2019-04-06 16:29:12 +0800

    ubah index,form,css

--- form.js
+++ form.js
@@ -1,51 +1,107 @@
+var barisRatingNovel=new Array(1,1,1,1);
+var barisPenghargaan=new Array(1,1,1,1);
+var barisPopularitas=new Array(1,1,1,1);
+var barisRatingPenulis=new Array(1,1,1,1);
+var el1;
+var el2;
 function printNilai(value,sliderKe){
     var nilai;
-    //mencari perbandingan slider
-    var el1,el2;
+    var stringKalimat;
     if(sliderKe==1){
-        el1="Rating Novel";
-        el2="Penghargaan";
+        el1="Rating Novel ";
+        el2="Penghargaan ";
+        //mencari nilai slider & mendapatkan string kalimat
+    if(value==9){
+                barisRatingNovel[1]==1;
+                stringKalimat= membentukString(el1,el2,nilai);
+                barisPenghargaan[0]=barisRatingNovel[0]/barisRatingNovel[1];}
+        else{
+    if(value<9){barisRatingNovel[1]=9-value;
+               stringKalimat= membentukString(el2,el1,nilai);
+               barisPenghargaan[0]}
+    else{barisRatingNovel[1]=value-8;
+               stringKalimat= membentukString(el1,el2,nilai);}
+        }
+        //Mengubah matriks
+        barisPenghargaan[0]=barisRatingNovel[0]/barisRatingNovel[1];
     }
     if(sliderKe==2){
-        el1="Rating Novel";
-        el2="Popularitas Penulis";
+        el1="Rating Novel ";
+        el2="Popularitas Penulis ";
+        barisRatingNovel[2]=cariNilaiSlider(value,sliderKe);
+        barisPopularitas[0]=barisRatingNovel[0]/barisRatingNovel[2];
     }
     if(sliderKe==3){
-        el1="Rating Novel";
-        el2="Rating Penulis";
+        el1="Rating Novel ";
+        el2="Rating Penulis ";
+        barisRatingNovel[3]=cariNilaiSlider(value,sliderKe);
+        barisPopularitas[1]=barisPenghargaan[1]/barisPenghargaan[2];
     }
     if(sliderKe==4){
-        el1="Penghargaan";
-        el2="Popularitas Penulis";
+        el1="Penghargaan ";
+        el2="Popularitas Penulis ";
+        barisPenghargaan[2]=cariNilaiSlider(value,sliderKe);
+        barisRatingPenulis[0]=barisRatingNovel[0]/barisRatingNovel[3];
     }
     if(sliderKe==5){
-        el1="Penghargaan";
-        el2="Rating Penulis";
+        el1="Penghargaan ";
+        el2="Rating Penulis ";
+        barisPenghargaan[3]=cariNilaiSlider(value,sliderKe);
+        barisRatingPenulis[1]=barisPenghargaan[1]/barisPenghargaan[3];
     }
     if(sliderKe==6){
-        el1="Popularitas Penulis";
-        el2="Rating Penulis";
+        el1="Popularitas Penulis ";
+        el2="Rating Penulis ";
+        barisPopularitas[3]=cariNilaiSlider(value,sliderKe);
+        barisRatingPenulis[2]=barisPopularitas[2]/barisPopularitas[3];
     }
-    //mencari nilai slider
-    if(value==9){nilai=1;
-                perbandinganSlider(a,b,nilai);
-                }else{
-    if(value<9){nilai=9-value;
-               }
-    else{nilai=value-8;
-        }}
     //print nilai
-     document.getElementById("nilaiSlider"+sliderKe).innerHTML=nilai;
+    document.getElementById("kataSlider"+sliderKe).innerHTML=stringKalimat;
+    document.getElementById("nilaiSlider"+sliderKe).innerHTML=nilai;
+    //print matriks
+    printMatriks();
 }
-function printPerbandingan(a,b,nilai){
-    var iK1="sama pentingnya";
-    var iK3="sedikit lebih penting";
-    var iK5="lebih penting";
-    var iK7="kuat penting";
-    var iK9="sangat penting";
-    var iK2="antara"+iK1+"dan"+iK3;
-    var iK4="antara"+iK3+"dan"+iK5;
-    var iK6="antara"+iK5+"dan"+iK7;
-    var iK8="antara"+iK7+"dan"+iK9;
+function cariNilaiSlider(value,sliderKe){
     
+}
+function membentukString(el1,el2,nilai){
+    var iK1="<span style='color:blue'>sama penting </span>";
+    var iK3="<span style='color:blue'>sedikit lebih penting </span>";
+    var iK5="<span style='color:blue'>lebih penting </span>";
+    var iK7="<span style='color:blue'>kuat penting </span>";
+    var iK9="<span style='color:blue'>sangat penting </span>";
+    var iK2="antara "+iK1+"dan "+iK3;
+    var iK4="antara "+iK3+"dan "+iK5;
+    var iK6="antara "+iK5+"dan "+iK7;
+    var iK8="antara "+iK7+"dan "+iK9;
+    var stringKalimat;
+    if(nilai==1){
+        stringKalimat=el1+iK1+"dengan "+el2;}
+    if(nilai==2){
+        stringKalimat=el1+iK2+"dengan "+el2;}
+    if(nilai==3){
+        stringKalimat=el1+iK3+"dengan "+el2;}
+    if(nilai==4){
+        stringKalimat=el1+iK4+"dengan "+el2;}
+    if(nilai==5){
+        stringKalimat=el1+iK5+"dengan "+el2;}
+    if(nilai==6){
+        stringKalimat=el1+iK6+"dengan "+el2;}
+    if(nilai==7){
+        stringKalimat=el1+iK7+"dengan "+el2;}
+    if(nilai==8){
+        stringKalimat=el1+iK8+"dengan "+el2;}
+    if(nilai==9){
+        stringKalimat=el1+iK9+"dengan "+el2;}
+    return stringKalimat;
+}
+
+function printMatriks(){
+   for(var i=0;i<4;i++){
+       var index=i+1;
+ document.getElementById("barisRatingNovel"+(i+1)).innerHTML=barisRatingNovel[i];
+   document.getElementById("barisPenghargaan"+(i+1)).innerHTML=barisPenghargaan[i];
+       document.getElementById("barisPopularitasPenulis"+(i+1)).innerHTML=barisPopularitas[i];
+       document.getElementById("barisRatingPenulis"+(i+1)).innerHTML=barisRatingPenulis[i];
+   }
 }
--- index.php
+++ index.php
@@ -9,37 +9,77 @@
         <h1>Tentukan Prioritas Kriteria</h1>
             
         <h2>Antara Rating Novel dengan Penghargaan</h2>
-        <h3 id="kataSlider1"></h3>
-        <input id="slider1" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="hitungNilaiSlider(this.value,1)" onChange="hitungNilaiSlider(this.value,1)">
+            <h3 id="kataSlider1">Rating Novel <span style='color:blue'>sama penting</span> dengan Penghargaan</h3>
+        <input id="slider1" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="printNilai(this.value,1)" onChange="printNilai(this.value,1)">
         <span id="nilaiSlider1">1</span>
         
         <h2>Antara Rating Novel dengan Popularitas Penulis</h2>
-        <h3 id="kataSlider2"></h3>
-        <input id="slider2" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="hitungNilaiSlider(this.value,2)" onChange="hitungNilaiSlider(this.value,2)">
+            <h3 id="kataSlider2">Rating Novel <span style='color:blue'>sama penting</span> dengan Popularitas Penulis</h3>
+        <input id="slider2" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="printNilai(this.value,2)" onChange="printNilai(this.value,2)">
         <span id="nilaiSlider2">1</span>
         
         <h2>Antara Rating Novel dengan Rating Penulis</h2>
-        <h3 id="kataSlider3"></h3>
-        <input id="slider3" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="hitungNilaiSlider(this.value,3)" onChange="hitungNilaiSlider(this.value,3)">
+            <h3 id="kataSlider3">Rating Novel <span style='color:blue'>sama penting</span> dengan Rating Penulis</h3>
+        <input id="slider3" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="printNilai(this.value,3)" onChange="printNilai(this.value,3)">
         <span id="nilaiSlider3">1</span>
             
         <h2>Antara Penghargaan dengan Popularitas Penulis</h2>
-        <h3 id="kataSlider4"></h3>
-        <input id="slider4" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="hitungNilaiSlider(this.value,4)" onChange="hitungNilaiSlider(this.value,4)">
+            <h3 id="kataSlider4">Penghargaan <span style='color:blue'>sama penting</span> dengan Popularitas Penulis</h3>
+        <input id="slider4" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="printNilai(this.value,4)" onChange="printNilai(this.value,4)">
         <span id="nilaiSlider4">1</span>
             
         <h2>Antara Penghargaan dengan Rating Penulis</h2>
-        <h3 id="kataSlider5"></h3>
-        <input id="slider5" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="hitungNilaiSlider(this.value,5)" onChange="hitungNilaiSlider(this.value,5)">
+            <h3 id="kataSlider5">Penghargaan <span style='color:blue'>sama penting</span> dengan Rating Penulis</h3>
+        <input id="slider5" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="printNilai(this.value,5)" onChange="printNilai(this.value,5)">
         <span id="nilaiSlider5">1</span>
             
         <h2>Antara Popularitas Penulis dengan Rating Penulis</h2>
-        <h3 id="kataSlider6"></h3>
-        <input id="slider6" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="hitungNilaiSlider(this.value,6)" onChange="hitungNilaiSlider(this.value,6)">
+            <h3 id="kataSlider6">Popularitas Penulis <span style='color:blue'>sama penting</span> dengan Rating Penulis</h3>
+        <input id="slider6" name="inputJudulSkenario" type="range" value="9" min="0" max="17" class="slider" onmousemove="printNilai(this.value,6)" onChange="printNilai(this.value,6)">
         <span id="nilaiSlider6">1</span>
+        <p></p>
             
-        <h2></h2>
-        <input type="submit" name="submit" value="Tampilkan Rekomendasi">
+            <div id="matriksInput">
+                <table style="width:80%">
+                    <tr>
+                    <th></th>
+                    <th>Rating Novel</th>
+                    <th>Penghargaan</th>
+                    <th>Popularitas Penulis</th>
+                    <th>Rating Penulis</th>
+                    </tr>
+                    <tr>
+                    <th>Rating Novel</th>
+                    <th id="barisRatingNovel1">1</th>
+                    <th id="barisRatingNovel2">1</th>
+                    <th id="barisRatingNovel3">1</th>
+                    <th id="barisRatingNovel4">1</th>
+                    <hr>
+                    </tr>
+                    <tr>
+                    <th>Penghargaan</th>
+                    <th id="barisPenghargaan1">1</th>
+                    <th id="barisPenghargaan2">1</th>
+                    <th id="barisPenghargaan3">1</th>
+                    <th id="barisPenghargaan4">1</th>
+                    </tr>
+                    <tr>
+                    <th>Popularitas Penulis</th>
+                    <th id="barisPopularitasPenulis1">1</th>
+                    <th id="barisPopularitasPenulis2">1</th>
+                    <th id="barisPopularitasPenulis3">1</th>
+                    <th id="barisPopularitasPenulis4">1</th>
+                    </tr>
+                    <tr>
+                    <th>Rating Penulis</th>
+                    <th id="barisRatingPenulis1">1</th>
+                    <th id="barisRatingPenulis2">1</th>
+                    <th id="barisRatingPenulis3">1</th>
+                    <th id="barisRatingPenulis4">1</th>
+                    </tr>
+                </table>
+            </div>
+        <input type="submit" name="submit" value="Tampilkan Rekomendasi" onclick="">
         </form>
         </div>
     
