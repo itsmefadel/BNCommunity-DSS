b0dd75b2150d96bdc0482c69ea0f0fe99ead8e3d
Fadel Harris <itsmefadel@gmail.com>
2019-04-11 00:09:56 +0800

    tambah data asli.js

---
+++ dataAsli.js
@@ -0,0 +1,16 @@
+var dataRaw{
+    'baris1':{
+        'judul':'the fault in our stars',
+        'ratingNovel':'4.24',
+        'penghargaan':'27',
+        'popularitasPenulis':'260439',
+        'ratingPenulis':'4.08'
+    },
+    'baris2':{
+        'judul':'the fault in our stars',
+        'ratingNovel':'4.24',
+        'penghargaan':'27',
+        'popularitasPenulis':'260439',
+        'ratingPenulis':'4.08'
+    }
+}
